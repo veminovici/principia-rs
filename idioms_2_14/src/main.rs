6f0bb0f7030f522d625e373752b091b1d9380ca8
VLAD EMINOVICI <veminovici@hotmail.com>
2022-03-22 19:52:29 +0200

    add idiom 14

---
+++ idioms_2_14/src/main.rs
@@ -0,0 +1,25 @@
+// Temporary mutability
+
+
+// nested block
+
+// let data = {
+//     let mut data = get_vec();
+//     data.sort();
+//     data
+// }
+
+// Here data is immutable
+
+// using variable rebinding
+
+// let mut data = get)vec();
+// data.sort();
+// let data = data;
+
+// Here data is immutable
+
+
+fn main() {
+    println!("Hello, world!");
+}
