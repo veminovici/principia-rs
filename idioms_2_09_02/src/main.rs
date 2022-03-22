396ee4c4cc944cf393aaa9f72cb3b6bcabd98c6d
VLAD EMINOVICI <veminovici@hotmail.com>
2022-03-22 17:37:22 +0200

    add idiom 9.2

---
+++ idioms_2_09_02/src/main.rs
@@ -0,0 +1,22 @@
+// CStr
+
+// pub mod unsafe_module {
+//     #[no_mangle]
+//     pub unsafe extern "C" fn mylib_log(msg: *const libc::c_char, level: libc::c_int) {
+//         let level: crate::LogLevel = match level {};
+
+//         let msg_str: &str = match std::ffi::CStr::from_ptr(msg).to_str() {
+//             Ok(s) => s,
+//             Err(e) => {
+//                 crate::log_err("FFI string conversion failed");
+//                 returnl
+//             }
+//         };
+
+//         crate::log(msg_str, level);
+//     }
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
