55d86b78e639d21c0a5ed0d19db7a8c033e6080b
VLAD EMINOVICI <veminovici@hotmail.com>
2022-03-22 13:42:43 +0200

    add idioms 7

---
+++ idioms_2_07/src/main.rs
@@ -0,0 +1,36 @@
+// An enum with two variants.
+
+use std::mem;
+
+enum MyEnum {
+    A { name: String, x: u8 },
+    B { name: String }
+}
+
+fn a_to_b(e: &mut MyEnum) {
+    if let MyEnum::A { name, x: 0} = e {
+        *e = MyEnum::B {name: mem::take(name)}
+    }
+}
+
+enum MyEnum1 {
+    A { name: String },
+    B { name: String },
+    C,
+    D
+}
+
+fn swizzle(e: &mut MyEnum1) {
+    use MyEnum1::*;
+
+    *e = match e {
+        A { name } => B { name: mem::take(name)},
+        B { name } => A { name: mem::take(name)},
+        C => D,
+        D => C,
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
