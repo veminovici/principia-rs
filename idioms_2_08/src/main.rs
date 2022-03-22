6e1231fff5eaa845d711b12baab0610b09989784
VLAD EMINOVICI <veminovici@hotmail.com>
2022-03-22 15:31:18 +0200

    add idion 8

---
+++ idioms_2_08/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use std::fs;
+fn main() {
+    let arg = "test";
+
+    // These must live linger than 
+    let (mut stdin_read, mut file_read);
+
+    let _readable: &mut dyn io::Read = if arg == "-"  {
+        stdin_read = io::stdin();
+        &mut stdin_read
+    } else {
+        file_read = fs::File::open(arg).unwrap();
+        &mut file_read
+    };
+
+    // use readable here
+
+    println!("Hello, world!");
+}
