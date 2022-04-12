d301e5a5b01d06ae9f035a5a9d2eb9f662056e9c
VLAD EMINOVICI <veminovici@hotmail.com>
2022-04-12 22:42:15 +0300

    Add command patter using functions

---
+++ behaviour/command/command_function/src/main.rs
@@ -0,0 +1,47 @@
+type FnPtr = fn() -> String;
+
+struct Command {
+    execute: FnPtr,
+    rollback: FnPtr,
+}
+
+struct Schema {
+    commands: Vec<Command>,
+}
+
+impl Schema {
+    pub fn new() -> Self {
+        Self { commands: vec![] }
+    }
+
+    pub fn add_migration(&mut self, execute: FnPtr, rollback: FnPtr) {
+        self.commands.push(Command { execute, rollback });
+    }
+
+    pub fn execute(&self) -> Vec<String> {
+        self.commands.iter().map(|cmd| (cmd.execute)()).collect()
+    }
+
+    pub fn rollback(&self) -> Vec<String> {
+        self.commands
+            .iter()
+            .rev()
+            .map(|cmd| (cmd.rollback)())
+            .collect()
+    }
+}
+fn add_field() -> String {
+    "add field".to_string()
+}
+
+fn remove_field() -> String {
+    "remove field".to_string()
+}
+
+fn main() {
+    let mut schema = Schema::new();
+    schema.add_migration(|| "create table".to_string(), || "drop table".to_string());
+    schema.add_migration(add_field, remove_field);
+    assert_eq!(vec!["create table", "add field"], schema.execute());
+    assert_eq!(vec!["remove field", "drop table"], schema.rollback());
+}
